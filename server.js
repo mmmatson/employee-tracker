36d8ca03f029218be9548feb0de588f66d543854
mmm <margaret.m.matson@gmail.com>
2022-12-15 20:09:32 -0600

    added inquirer questions

---
+++ server.js
@@ -0,0 +1,94 @@
+//Packages needed for this application
+const inquirer = require("inquirer");
+
+
+//Questions to prompt user for input
+const trackerQuestions = () => {
+    return inquirer.prompt([
+        {
+            name: "options",
+            type: "list",
+            message: "Choose an option:",
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee's role"],
+        },
+        //Add department questions
+        {
+            when: input => { return input.options === "Add a department" },
+            name: "deptName",
+            type: "input",
+            message: "What is the department's name?",
+        },
+        //Add role questions
+        {
+            when: input => { return input.options === "Add a role" },
+            name: "roleName",
+            type: "input",
+            message: "What is the role's name?",
+        },
+        {
+            when: input => { return input.options === "Add a role" },
+            name: "roleSalary",
+            type: "input",
+            message: "What is the role's salary?",
+        },
+        {
+            when: input => { return input.options === "Add a role" },
+            name: "roleDept",
+            type: "list",
+            message: "What is the department for the role?",
+            choices: [1, 2]//[`SELECT * FROM departments`]
+        },
+        //Add employee questions
+        {
+            when: input => { return input.options === "Add an employee" },
+            name: "empFirstName",
+            type: "input",
+            message: "What is the employee's first name?",
+        },
+        {
+            when: input => { return input.options === "Add an employee" },
+            name: "empLastName",
+            type: "input",
+            message: "What is the employee's last name?",
+        },
+        {
+            when: input => { return input.options === "Add an employee" },
+            name: "empRole",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: [1, 2]//[`SELECT * FROM roles`]
+        },
+        {
+            when: input => { return input.options === "Add an employee" },
+            name: "empManager",
+            type: "input",
+            message: "Who is the employee's manager?",
+        },
+        //Update employee's role question
+        {
+            when: input => { return input.options === "Update an employee's role" },
+            name: "empSelect",
+            type: "list",
+            message: "Choose an option:",
+            choices: [1, 2]//[`SELECT * FROM employees`]
+        },
+        {
+            when: input => { return input.options === "Update an employee's role" },
+            name: "empRole",
+            type: "input",
+            message: "What is the employee's role?",
+        }
+    ])
+        .then(answers => {
+            console.log(answers);
+            return answers;
+        });
+};
+
+//Define function to initialize app
+function init() {
+    trackerQuestions()
+}
+
+// Call function to initialize app
+init();
