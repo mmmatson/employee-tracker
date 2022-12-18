45091d3372c0c768fcfd9006eb9e2c7af0ba653b
mmm <margaret.m.matson@gmail.com>
2022-12-18 09:07:43 -0600

    added code to connect to database

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 //Packages needed for this application
 const inquirer = require("inquirer");
+const mysql = require('mysql2');
 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'mmm333',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee database.`)
+);
 
 //Questions to prompt user for input
 const trackerQuestions = () => {
